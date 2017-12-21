c8a224a7011b69f9b7d3c2a22fe0bcef5169eb6e
David Parra Perez <david.parra@outlook.com>
2017-12-21 14:46:41 +0100

    default go project

--- src/config.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-var setty = require('setty');
-setty.load({settingsDir: path.join(__dirname, './settings')});
-
-module.exports = {
-    env: setty.get("env"),
-    port: setty.get("port")
-};
