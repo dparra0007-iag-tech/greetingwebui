951ec2f1acf254d45116f6ecc250b0f293af4dde
David Parra Perez <david.parra@outlook.com>
2018-01-01 17:02:14 +0100

    Customize build dependencies for ownership

--- gofrontend/Scripts/vagrant.sh
+++ gofrontend/Scripts/vagrant.sh
@@ -2,6 +2,6 @@
 
 mkdir /home/packer/.ssh
 wget --no-check-certificate \
-    'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
+    'https://github.com/dparra0007/vagrant/blob/master/keys/vagrant.pub' \
     -O /home/packer/.ssh/authorized_keys
 chown -R packer /home/packer/.ssh
