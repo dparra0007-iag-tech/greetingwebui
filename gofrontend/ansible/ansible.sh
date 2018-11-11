a2d34077ceb84c165213a81390a8d31b4714cee3
david-iaggbs <david.parra@iaggbs.com>
2018-11-11 15:15:11 +0100

    enable docekr builder for packer

--- gofrontend/ansible/ansible.sh
+++ gofrontend/ansible/ansible.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
+sudo apt-get update
 sudo apt-get install -y software-properties-common
+
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
